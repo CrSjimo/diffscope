bce01be4f711dcb99a748da0f4b83d527eca489a
CrSjimo <crs_16423@outlook.com>
2024-04-06 00:19:37 +0800

    update

--- scripts/setup/Clone-DiffScope-Bridge.ps1
+++
@@ -1,7 +0,0 @@
-$repository = 'git@github.com:CrSjimo/diffscope-bridge.git'
-$branch = 'master'
-$commit = '941f82380a833bc6aa04f57328b77e46d7caf5c9'
-
-git clone $repository -b $branch --recursive
-Set-Location $(Split-Path -Path $repository -LeafBase)
-git checkout $commit
---
+++ scripts/setup/Get-DiffScope-Bridge-Target.ps1
@@ -0,0 +1,4 @@
+Write-Output @{
+    repository = 'CrSjimo/diffscope-bridge'
+    ref = '941f82380a833bc6aa04f57328b77e46d7caf5c9'
+}
