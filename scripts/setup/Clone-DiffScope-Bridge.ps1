20f3d1d89f4021465df35b3dc088f5244e4f967f
CrSjimo <crs_16423@outlook.com>
2024-04-05 23:47:06 +0800

    update

--- scripts/setup/Clone-DiffScope-Bridge.ps1
+++ scripts/setup/Clone-DiffScope-Bridge.ps1
@@ -1,4 +1,4 @@
-$repository = 'https://github.com/CrSjimo/diffscope-bridge.git'
+$repository = 'git@github.com:CrSjimo/diffscope-bridge.git'
 $branch = 'master'
 $commit = '941f82380a833bc6aa04f57328b77e46d7caf5c9'
 
